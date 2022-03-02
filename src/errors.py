be1a8d51743d8c6ec7effca92d83be39f569aa97
diegocoelhods <diegocoelho.13@gmail.com>
2022-03-02 19:51:02 -0300

    adding more logs

--- src/errors.py
+++ src/errors.py
@@ -1,25 +1,33 @@
 from flask import Blueprint, jsonify
 from src.constants.http_codes import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from logging import getLogger
 
 errors = Blueprint('errors', __name__)
+log = getLogger(__name__)
 
 
 @errors.app_errorhandler(HTTP_401_UNAUTHORIZED)
 def error_401(error):
-    return jsonify({'error': {
+    output = {'error': {
         'reason': 'something went wrong, please try again'
-    }}), HTTP_500_INTERNAL_SERVER_ERROR
+    }}
+    log.info('{}\t{}'.format(HTTP_401_UNAUTHORIZED, output))
+    return jsonify(output), HTTP_401_UNAUTHORIZED
 
 
 @errors.app_errorhandler(HTTP_404_NOT_FOUND)
 def error_404(error):
-    return jsonify({'error': {
+    output = {'error': {
         'reason': 'page not found'
-    }}), HTTP_404_NOT_FOUND
+    }}
+    log.info('{}\t{}'.format(HTTP_404_NOT_FOUND, output))
+    return jsonify(), HTTP_404_NOT_FOUND
 
 
 @errors.app_errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
 def error_500(error):
-    return jsonify({'error': {
+    output = {'error': {
         'reason': 'something went wrong, please try again'
-    }}), HTTP_500_INTERNAL_SERVER_ERROR
+    }}
+    log.info('{}\t{}'.format(HTTP_401_UNAUTHORIZED, output))
+    return jsonify(output), HTTP_500_INTERNAL_SERVER_ERROR
